c531705fb61b738153874303c3479849d6b17f76
monipmark <90512420+monipmark@users.noreply.github.com>
2023-07-19 19:52:21 +0700

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author "ALIEN DEV CAR"
+
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta',
+	'dlctext.meta',
+
+}
+ 
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'TEXTFILE_METAFILE' 'dlctext.META'
+
+ 
